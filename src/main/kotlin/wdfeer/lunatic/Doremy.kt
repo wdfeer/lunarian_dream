4a343a7f1030585e509b0adabefb2b0c943e6d18
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-20 07:34:55 +0100

    Interact on right-click

--- src/main/kotlin/wdfeer/lunatic/Doremy.kt
+++ src/main/kotlin/wdfeer/lunatic/Doremy.kt
@@ -5,16 +5,18 @@ import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.SpawnGroup
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.resource.featuretoggle.FeatureSet
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.world.World
 import org.ladysnake.blabber.Blabber
-import java.util.*
 
 fun Lunatic.initializeDoremyEntity() {
     Registry.register(Registries.ENTITY_TYPE, Identifier.of(MOD_ID, "doremy"), DoremyEntityType)
@@ -30,29 +32,15 @@ class Doremy(world: World) : Entity(DoremyEntityType, world) {
     override fun readCustomDataFromNbt(nbt: NbtCompound?) {}
     override fun writeCustomDataToNbt(nbt: NbtCompound?) {}
 
-    private val playersTalkingTo = mutableListOf<Pair<UUID, Long>>()
-    override fun tick() {
-        if ((world.time % 20).toInt() == 0) {
-            world.players.filterIsInstance<ServerPlayerEntity>()
-                .filter { it.distanceTo(this) < 6 }
-                .filter {
-                    // Looking at doremy
-                    it.rotationVector.normalize().subtract(eyePos.subtract(it.eyePos).normalize()).length() < 0.1
-                }
-                .filter { player ->
-                    playersTalkingTo.none { it.first == player.uuid }
-                }
-                .forEach {
-                    Blabber.startDialogue(it, Identifier.of(Lunatic.MOD_ID, "doremy"))
-                    playersTalkingTo += it.uuid to world.time
-                }
+    override fun interact(player: PlayerEntity?, hand: Hand?): ActionResult =
+        if (player is ServerPlayerEntity) {
+            Blabber.startDialogue(player, Identifier.of(Lunatic.MOD_ID, "doremy"))
+            ActionResult.SUCCESS
+        } else ActionResult.PASS
 
-            playersTalkingTo.removeIf { world.time - it.second > 240 }
-        }
-
-
-        super.tick()
-    }
+    // These are required for interactability
+    override fun isCollidable(): Boolean = true
+    override fun canHit(): Boolean = true
 }
 
 object DoremyEntityType : EntityType<Doremy>(
