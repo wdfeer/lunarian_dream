927692481414781dc8e75167fcbd813ad91df642
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-24 22:38:06 +0100

    Refactor sky render

--- src/client/kotlin/wdfeer/lunatic/DreamWorldSkyRenderer.kt
+++ src/client/kotlin/wdfeer/lunatic/DreamWorldSkyRenderer.kt
@@ -9,12 +9,10 @@ import org.joml.Matrix4f
 import org.joml.Vector3f
 import kotlin.math.*
 
-const val PIf = PI.toFloat()
 
 object DreamWorldSkyRenderer : SkyRenderer {
     override fun render(ctx: WorldRenderContext) {
         val matrixStack = ctx.matrixStack()
-        val tickDelta = ctx.tickDelta()
 
         RenderSystem.setShader { GameRenderer.getPositionColorProgram() }
         RenderSystem.enableBlend()
@@ -25,20 +23,19 @@ object DreamWorldSkyRenderer : SkyRenderer {
         val buffer = tessellator.buffer
         val matrices = matrixStack.peek().positionMatrix
 
-        drawLineEffect(matrices, buffer)
+        drawLineEffect(matrices, buffer, MinecraftClient.getInstance().player?.y?.toFloat() ?: 60f)
         tessellator.draw()
 
         RenderSystem.depthMask(true)
         RenderSystem.disableBlend()
     }
 
-    private fun drawLineEffect(matrices: Matrix4f, buffer: BufferBuilder) {
+    private fun drawLineEffect(matrices: Matrix4f, buffer: BufferBuilder, playerY: Float) {
         buffer.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR)
 
         val count = 17
         val x = 3000f
         val y = 10f
-        val playerY = MinecraftClient.getInstance().player?.y?.toFloat() ?: 60f
         for (z in (1..count).map { i ->
             (i - count / 2f) * (sqrt(playerY) + 1f)
         }) {
