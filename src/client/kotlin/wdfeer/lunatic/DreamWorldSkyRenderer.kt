7a8caebb186c5ee393f56b980852bc0853956879
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-25 09:28:06 +0100

    Create grid effect

--- src/client/kotlin/wdfeer/lunatic/DreamWorldSkyRenderer.kt
+++ src/client/kotlin/wdfeer/lunatic/DreamWorldSkyRenderer.kt
@@ -23,24 +23,29 @@ object DreamWorldSkyRenderer : SkyRenderer {
         val buffer = tessellator.buffer
         val matrices = matrixStack.peek().positionMatrix
 
-        drawLineEffect(matrices, buffer, MinecraftClient.getInstance().player?.y?.toFloat() ?: 60f)
+        drawGridEffect(matrices, buffer, MinecraftClient.getInstance().player?.y?.toFloat() ?: 60f)
         tessellator.draw()
 
         RenderSystem.depthMask(true)
         RenderSystem.disableBlend()
     }
 
-    private fun drawLineEffect(matrices: Matrix4f, buffer: BufferBuilder, playerY: Float) {
+    private fun drawGridEffect(matrices: Matrix4f, buffer: BufferBuilder, playerY: Float) {
         buffer.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR)
 
-        val count = 17
+        val count = 37
         val x = 3000f
         val y = 10f
         for (z in (1..count).map { i ->
-            (i - count / 2f) * (sqrt(playerY) + 1f)
+            (i - count / 2f) * 2f * (sqrt(playerY) + 0.5f)
         }) {
-            buffer.line(matrices, Vector3f(-x, y, z), Vector3f(x, y, z))
-            buffer.line(matrices, Vector3f(-x, -y, z), Vector3f(x, -y, z))
+            // +X to -X top and bottom
+            buffer.line(matrices, Vector3f(x, y, z), Vector3f(-x, y, z))
+            buffer.line(matrices, Vector3f(x, -y, z), Vector3f(-x, -y, z))
+
+            // +Z to -Z top and bottom
+            buffer.line(matrices, Vector3f(z, y, x), Vector3f(z, y, -x))
+            buffer.line(matrices, Vector3f(z, -y, x), Vector3f(z, -y, -x))
         }
     }
 
