35d328eabfb3ed261cf422f3730baba930126e2d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-25 10:00:09 +0100

    Create dream_portal data

--- src/main/kotlin/wdfeer/lunatic/ModBlocks.kt
+++ src/main/kotlin/wdfeer/lunatic/ModBlocks.kt
@@ -7,18 +7,17 @@ import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
 fun initializeBlocks() {
-    ModBlocks
+    DreamPortal
 }
 
-private object ModBlocks {
-    val dreamPortal = Block(
-        FabricBlockSettings.create()
-            .nonOpaque()
-            .notSolid()
-            .hardness(Float.POSITIVE_INFINITY)
-            .resistance(Float.POSITIVE_INFINITY)
-            .luminance { 15 }
-    ).also {
-        Registry.register(Registries.BLOCK, Identifier(Lunatic.MOD_ID, "dream_portal"), it)
+private object DreamPortal : Block(
+    FabricBlockSettings.create()
+        .nonOpaque()
+        .notSolid()
+        .hardness(Float.POSITIVE_INFINITY)
+        .resistance(Float.POSITIVE_INFINITY)
+        .luminance { 15 }) {
+    init {
+        Registry.register(Registries.BLOCK, Identifier(Lunatic.MOD_ID, "dream_portal"), this)
     }
 }
