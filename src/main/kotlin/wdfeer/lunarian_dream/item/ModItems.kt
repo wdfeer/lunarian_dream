0f9119784c368cb152e659a4b9c6f5d9fc5e37ba
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-02-03 18:36:18 +0100

    Implement GravityController func

--- src/main/kotlin/wdfeer/lunarian_dream/item/ModItems.kt
+++ src/main/kotlin/wdfeer/lunarian_dream/item/ModItems.kt
@@ -1,11 +1,18 @@
 package wdfeer.lunarian_dream.item
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.util.Rarity
+import net.minecraft.util.TypedActionResult
+import net.minecraft.world.World
 import wdfeer.lunarian_dream.LunarianDream
 
 fun LunarianDream.initializeItems() {
@@ -14,4 +21,11 @@ fun LunarianDream.initializeItems() {
 }
 
 private object NightmareShard : Item(FabricItemSettings().rarity(Rarity.RARE).fireproof())
-private object GravityController : Item(FabricItemSettings().rarity(Rarity.RARE).fireproof())
+
+private object GravityController : Item(FabricItemSettings().rarity(Rarity.RARE).fireproof()) {
+    override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
+        user?.itemCooldownManager?.set(this, 60)
+        user?.addStatusEffect(StatusEffectInstance(StatusEffects.LEVITATION, 30, 1))
+        return TypedActionResult.success(user?.getStackInHand(hand))
+    }
+}
