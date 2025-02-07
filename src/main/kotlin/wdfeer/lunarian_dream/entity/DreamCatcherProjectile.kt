efb279fff65c8c667814f9e962b34eba97fad4d4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-02-07 18:12:50 +0100

    Make DreamCatcherProjectile deal damage

--- src/main/kotlin/wdfeer/lunarian_dream/entity/DreamCatcherProjectile.kt
+++ src/main/kotlin/wdfeer/lunarian_dream/entity/DreamCatcherProjectile.kt
@@ -1,13 +1,17 @@
 package wdfeer.lunarian_dream.entity
 
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.SpawnGroup
 import net.minecraft.entity.projectile.ProjectileEntity
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
+import net.minecraft.util.hit.EntityHitResult
+import net.minecraft.util.hit.HitResult
 import net.minecraft.world.World
 import wdfeer.lunarian_dream.LunarianDream
+import wdfeer.lunarian_dream.item.DreamCatcher
 
 fun LunarianDream.initializeDreamCatcherProjectile() {
     Registry.register(Registries.ENTITY_TYPE, Identifier.of(MOD_ID, ID), type)
@@ -15,11 +19,30 @@ fun LunarianDream.initializeDreamCatcherProjectile() {
 
 private const val ID = "dream_catcher_projectile"
 private val type: EntityType<DreamCatcherProjectile> =
-    EntityType.Builder.create({ _, world -> DreamCatcherProjectile(world) }, SpawnGroup.MISC).build(ID)
+    EntityType.Builder.create({ _, world -> DreamCatcherProjectile(world, null) }, SpawnGroup.MISC)
+        .setDimensions(0.5f, 0.5f).build(ID)
 
-class DreamCatcherProjectile(world: World) : ProjectileEntity(
+class DreamCatcherProjectile(world: World, shooter: LivingEntity?) : ProjectileEntity(
     type,
     world
 ) {
+    init {
+        owner = shooter
+        if (shooter != null) setVelocity(shooter, shooter.pitch, shooter.yaw, 0.0f, 1.5f, 0.5f)
+    }
+
+    override fun onCollision(hitResult: HitResult) {
+        if (hitResult is EntityHitResult) {
+            val target = hitResult.entity as? LivingEntity ?: return
+            if (target != owner) target.damage(world.damageSources.magic(), DreamCatcher.DAMAGE)
+
+            discard()
+        }
+
+        super.onCollision(hitResult)
+    }
+
+    override fun isCollidable(): Boolean = true
+
     override fun initDataTracker() {}
 }
--- src/main/kotlin/wdfeer/lunarian_dream/item/DreamCatcher.kt
+++ src/main/kotlin/wdfeer/lunarian_dream/item/DreamCatcher.kt
@@ -13,11 +13,12 @@ import wdfeer.lunarian_dream.entity.DreamCatcherProjectile
 
 object DreamCatcher : Item(FabricItemSettings().fireproof().rarity(Rarity.UNCOMMON)) {
     private const val COOLDOWN = 100
+    const val DAMAGE = 5f
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         return if (world !is ServerWorld) super.use(world, user, hand)
         else {
-            val projectile = DreamCatcherProjectile(world)
+            val projectile = DreamCatcherProjectile(world, user)
             projectile.velocity = user.rotationVector.multiply(9.0)
             world.spawnEntity(projectile)
             user.itemCooldownManager!!.set(this, COOLDOWN)
